6ac33fed0c6d122d530509496bd5fa4bc5c4de34
Nhahan <kisy324@naver.com>
2023-07-05 00:01:24 +0900

    chore: jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    testTimeout: 5000,
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+};
