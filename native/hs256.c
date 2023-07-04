0b14c5f81d1a580331dd8a377b027b44a4736f28
Nhahan <kisy324@naver.com>
2023-07-05 01:46:58 +0900

    feat: openssl

--- native/hs256.c
+++ native/hs256.c
@@ -1,111 +1,219 @@
 #include <node_api.h>
+#include <stdio.h>
 #include <string.h>
-#include <openssl/hmac.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
 
-typedef struct {
-    napi_deferred deferred;
-    napi_async_work work;
-    unsigned char* digest;
-    unsigned int digest_len;
-    char* message;
-    size_t message_len;
-    char* key;
-    size_t key_len;
-    int status;
-} hmac_request;
-
-void execute_hmac(napi_env env, void* data) {
-    hmac_request* req = (hmac_request*)data;
-    req->digest = HMAC(EVP_sha256(), req->key, req->key_len, (unsigned char*)req->message, req->message_len, NULL, &(req->digest_len));
-    req->status = (req->digest != NULL ? 0 : 1);
+void printErrorMessage(const char *message)
+{
+    fprintf(stderr, "Error: %s\n", message);
 }
 
-void complete_hmac(napi_env env, napi_status status, void* data) {
-    hmac_request* req = (hmac_request*)data;
+void encrypt(const char *plaintext, const char *key, char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
 
-    if (status == napi_ok) {
-        if (req->status == 0) {
-            napi_value result;
-            napi_create_external_arraybuffer(env, req->digest, req->digest_len, NULL, free, &result);
-            napi_resolve_deferred(env, req->deferred, result);
-        } else {
-            napi_value error;
-            napi_create_string_utf8(env, "Failed to compute HMAC", NAPI_AUTO_LENGTH, &error);
-            napi_reject_deferred(env, req->deferred, error);
-        }
-    } else {
-        napi_value error;
-        napi_create_string_utf8(env, "Failed to complete HMAC computation", NAPI_AUTO_LENGTH, &error);
-        napi_reject_deferred(env, req->deferred, error);
+    int len;
+    int ciphertext_len;
+
+    ctx = EVP_CIPHER_CTX_new();
+    if (!ctx)
+    {
+        printErrorMessage("Failed to create encryption context");
+        return;
     }
 
-    napi_delete_async_work(env, req->work);
-    free(req);
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (const unsigned char *)key, (const unsigned char *)"00000000000000000000000000000000"))
+    {
+        printErrorMessage("Failed to initialize encryption operation");
+        EVP_CIPHER_CTX_free(ctx);
+        return;
+    }
+
+    if (1 != EVP_EncryptUpdate(ctx, (unsigned char *)ciphertext, &len, (const unsigned char *)plaintext, strlen(plaintext)))
+    {
+        printErrorMessage("Failed to perform encryption");
+        EVP_CIPHER_CTX_free(ctx);
+        return;
+    }
+    ciphertext_len = len;
+
+    if (1 != EVP_EncryptFinal_ex(ctx, (unsigned char *)ciphertext + len, &len))
+    {
+        printErrorMessage("Failed to finalize encryption");
+        EVP_CIPHER_CTX_free(ctx);
+        return;
+    }
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
 }
 
-napi_value HS256(napi_env env, napi_callback_info info) {
+void decrypt(const char *ciphertext, const char *key, char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+    int plaintext_len = 0;
+
+    ctx = EVP_CIPHER_CTX_new();
+    if (!ctx)
+    {
+        printErrorMessage("Failed to create decryption context");
+        return;
+    }
+
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (const unsigned char *)key, (const unsigned char *)"00000000000000000000000000000000"))
+    {
+        printErrorMessage("Failed to initialize decryption operation");
+        EVP_CIPHER_CTX_free(ctx);
+        return;
+    }
+
+    BIO *bio = BIO_new(BIO_f_base64());
+    BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+    BIO *bmem = BIO_new_mem_buf(ciphertext, -1);
+    bmem = BIO_push(bio, bmem);
+
+    if (1 != EVP_DecryptUpdate(ctx, (unsigned char *)plaintext + plaintext_len, &len, NULL, 0))
+    {
+        printErrorMessage("Failed to perform decryption");
+        EVP_CIPHER_CTX_free(ctx);
+        BIO_free_all(bmem);
+        return;
+    }
+    plaintext_len += len;
+
+    if (1 != EVP_DecryptUpdate(ctx, (unsigned char *)plaintext + plaintext_len, &len, (unsigned char *)ciphertext, strlen(ciphertext)))
+    {
+        printErrorMessage("Failed to perform decryption");
+        EVP_CIPHER_CTX_free(ctx);
+        BIO_free_all(bmem);
+        return;
+    }
+    plaintext_len += len;
+
+    if (1 != EVP_DecryptFinal_ex(ctx, (unsigned char *)plaintext + plaintext_len, &len))
+    {
+        printErrorMessage("Failed to finalize decryption");
+        EVP_CIPHER_CTX_free(ctx);
+        BIO_free_all(bmem);
+        return;
+    }
+    plaintext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+    BIO_free_all(bmem);
+
+    plaintext[plaintext_len] = '\0';
+}
+
+napi_value Encrypt(napi_env env, napi_callback_info info)
+{
+    napi_status status;
     size_t argc = 2;
     napi_value argv[2];
-    napi_status status;
 
     status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
-    if (status != napi_ok) {
+    if (status != napi_ok)
+    {
         napi_throw_error(env, NULL, "Failed to parse arguments");
         return NULL;
     }
 
-    hmac_request* req = (hmac_request*)malloc(sizeof(hmac_request));
-
-    status = napi_get_value_string_utf8(env, argv[0], NULL, 0, &(req->message_len));
-    if (status != napi_ok || req->message_len > 256) {
-        free(req);
-        napi_throw_error(env, NULL, "Message is too long");
+    char message[256];
+    size_t message_len;
+    status = napi_get_value_string_utf8(env, argv[0], message, sizeof(message), &message_len);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Invalid message");
         return NULL;
     }
 
-    req->message = (char*)malloc(req->message_len + 1);
-    status = napi_get_value_string_utf8(env, argv[0], req->message, req->message_len + 1, &(req->message_len));
-    if (status != napi_ok) {
-        free(req->message);
-        free(req);
-        napi_throw_error(env, NULL, "Failed to get message");
+    char key[256];
+    size_t key_len;
+    status = napi_get_value_string_utf8(env, argv[1], key, sizeof(key), &key_len);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Invalid key");
         return NULL;
     }
 
-    status = napi_get_value_string_utf8(env, argv[1], NULL, 0, &(req->key_len));
-    if (status != napi_ok || req->key_len < 1 || req->key_len > 256) {
-        free(req->message);
-        free(req);
-        napi_throw_error(env, NULL, "Key is invalid");
+    char ciphertext[256];
+    encrypt(message, key, ciphertext);
+
+    napi_value result;
+    status = napi_create_string_utf8(env, ciphertext, NAPI_AUTO_LENGTH, &result);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Failed to create result string");
         return NULL;
     }
 
-    req->key = (char*)malloc(req->key_len + 1);
-    status = napi_get_value_string_utf8(env, argv[1], req->key, req->key_len + 1, &(req->key_len));
-    if (status != napi_ok) {
-        free(req->message);
-        free(req->key);
-        free(req);
-        napi_throw_error(env, NULL, "Failed to get key");
-        return NULL;
-    }
-
-    napi_value promise;
-    napi_value work_name;
-    napi_create_promise(env, &(req->deferred), &promise);
-
-    napi_create_string_utf8(env, "HMAC work", NAPI_AUTO_LENGTH, &work_name);
-
-    napi_create_async_work(env, NULL, work_name, execute_hmac, complete_hmac, req, &(req->work));
-    napi_queue_async_work(env, req->work);
-
-    return promise;
+    return result;
 }
 
-napi_value Init(napi_env env, napi_value exports) {
-    napi_value fn;
-    napi_create_function(env, NULL, 0, HS256, NULL, &fn);
-    napi_set_named_property(env, exports, "HS256", fn);
+napi_value Decrypt(napi_env env, napi_callback_info info)
+{
+    napi_status status;
+    size_t argc = 2;
+    napi_value argv[2];
+
+    status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Failed to parse arguments");
+        return NULL;
+    }
+
+    char ciphertext[256];
+    size_t ciphertext_len;
+    status = napi_get_value_string_utf8(env, argv[0], ciphertext, sizeof(ciphertext), &ciphertext_len);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Invalid ciphertext");
+        return NULL;
+    }
+
+    char key[256];
+    size_t key_len;
+    status = napi_get_value_string_utf8(env, argv[1], key, sizeof(key), &key_len);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Invalid key");
+        return NULL;
+    }
+
+    char plaintext[256];
+    decrypt(ciphertext, key, plaintext);
+
+    napi_value result;
+    status = napi_create_string_utf8(env, plaintext, NAPI_AUTO_LENGTH, &result);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Failed to create result string");
+        return NULL;
+    }
+
+    return result;
+}
+
+napi_value Init(napi_env env, napi_value exports)
+{
+    napi_status status;
+    napi_property_descriptor desc[] = {
+        {"encrypt", NULL, Encrypt, NULL, NULL, NULL, napi_default, NULL},
+        {"decrypt", NULL, Decrypt, NULL, NULL, NULL, napi_default, NULL},
+    };
+
+    status = napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    if (status != napi_ok)
+    {
+        napi_throw_error(env, NULL, "Failed to define properties");
+        return NULL;
+    }
+
     return exports;
 }
 
