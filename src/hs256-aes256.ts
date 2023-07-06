6b1714b476e9bff36006a9b03836fa6f98cc4516
Nhahan <kisy324@naver.com>
2023-07-07 01:24:06 +0900

    feat: hs256-aes256 module

---
+++ src/hs256-aes256.ts
@@ -0,0 +1,17 @@
+// hs256-aes256.ts
+const addon = require('./hs256-aes256.node');
+
+export function hmac(data: string, secret: string): string {
+    return addon.hmac(data, secret);
+}
+
+export function encrypt(message: string, key: string): string {
+    const encryptedBuffer = addon.encrypt(message, key);
+    return encryptedBuffer.toString('base64');
+}
+
+export function decrypt(encrypted: string, key: string): string {
+    const encryptedBuffer = Buffer.from(encrypted, 'base64');
+    const decryptedBuffer = addon.decrypt(encryptedBuffer, key);
+    return decryptedBuffer.toString('utf8');
+}
