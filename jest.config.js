b9fa391d5031caeb36364beaa5234bbbae167e98
Nhahan <kisy324@naver.com>
2023-07-08 03:45:43 +0900

    chore: .ts test settings

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     testEnvironment: 'node',
-    testTimeout: 5000,
     testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts'],
 };
