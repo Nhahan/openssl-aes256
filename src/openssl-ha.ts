468302160c7ae416a7dd0f5df119842062f4daac
Nhahan <kisy324@naver.com>
2023-07-08 03:24:46 +0900

    fix: function name

--- src/openssl-ha.ts
+++ src/openssl-ha.ts
@@ -1,16 +1,16 @@
 const addon = require('./openssl-ha.node');
 
-export function hmac(data: string, secret: string): string {
-    return addon.hmac(data, secret);
-}
-
-export function encrypt(message: string, key: string): string {
-    const encryptedBuffer = addon.encrypt(message, key);
+export function encryptAes256(message: string, key: string): string {
+    const encryptedBuffer = addon.encryptAes256(message, key);
     return encryptedBuffer.toString('base64');
 }
 
-export function decrypt(encrypted: string, key: string): string {
-    const encryptedBuffer = Buffer.from(encrypted, 'base64');
-    const decryptedBuffer = addon.decrypt(encryptedBuffer, key);
+export function decryptAes256(ciphertext: string, key: string): string {
+    const ciphertextBuffer = Buffer.from(ciphertext, 'base64');
+    const decryptedBuffer = addon.decryptAes256(ciphertextBuffer, key);
     return decryptedBuffer.toString('utf8');
+}
+
+export function encryptHs256(data: string, secret: string): string {
+    return addon.encryptHs256(data, secret);
 }
