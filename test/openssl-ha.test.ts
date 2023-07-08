8946f47826d8eddeea22b438259d5dfdd1b39768
Nhahan <kisy324@naver.com>
2023-07-09 02:48:44 +0900

    test: openssl-ha.ts

--- test/openssl-ha.test.ts
+++ test/openssl-ha.test.ts
@@ -1,69 +1,51 @@
 import { encryptAes256, decryptAes256, encryptHs256 } from '../src/openssl-ha';
 
-describe('Encryption and Hashing Functions', () => {
-
-    const message: string = 'Hello, World!';
-    const key: string = 'secretpassword';
-    const ciphertext: string = 'SOME_BASE64_ENCODED_CIPHERTEXT';
-    const secret: string = 'secretpassword';
+describe('OpenSSL-HA', () => {
+    const validMessage = 'Hello, World!';
+    const validKey = '0123456789abcdef0123456789abcdef';
 
     describe('encryptAes256', () => {
-        it('should encrypt the message with the given key', () => {
-            const encrypted: string = encryptAes256(message, key);
-            expect(encrypted).toBeTruthy();
-            expect(typeof encrypted).toBe('string');
+        test('should encrypt the message with a valid key', () => {
+            const encrypted = encryptAes256(validMessage, validKey);
+            expect(encrypted).toBeDefined();
+            expect(encrypted).not.toBe(validMessage);
         });
 
-        it('should throw an error if message is missing', () => {
+        test('should throw an error for invalid message', () => {
+            const invalidMessage = null;
             expect(() => {
-                encryptAes256(undefined, key);
-            }).toThrow('Missing message');
+                encryptAes256(invalidMessage, validKey);
+            }).toThrow('Invalid message');
         });
 
-        it('should throw an error if key is missing', () => {
+        test('should throw an error for invalid key', () => {
+            const invalidKey = null;
             expect(() => {
-                encryptAes256(message, undefined);
-            }).toThrow('Missing key');
+                encryptAes256(validMessage, invalidKey);
+            }).toThrow('Invalid key');
         });
     });
 
     describe('decryptAes256', () => {
-        it('should decrypt the ciphertext with the given key', () => {
-            const decrypted: string = decryptAes256(ciphertext, key);
-            expect(decrypted).toBeTruthy();
-            expect(typeof decrypted).toBe('string');
-        });
-
-        it('should throw an error if ciphertext is missing', () => {
-            expect(() => {
-                decryptAes256(undefined, key);
-            }).toThrow('Missing ciphertext');
-        });
-
-        it('should throw an error if key is missing', () => {
-            expect(() => {
-                decryptAes256(ciphertext, undefined);
-            }).toThrow('Missing key');
+        test('should decrypt the ciphertext with a valid key', () => {
+            const encrypted = encryptAes256(validMessage, validKey);
+            const decrypted = decryptAes256(encrypted, validKey);
+            expect(decrypted).toBe(validMessage);
         });
     });
 
     describe('encryptHs256', () => {
-        it('should generate the HMAC-SHA256 hash of the data using the secret', () => {
-            const hashed: string = encryptHs256(message, secret);
-            expect(hashed).toBeTruthy();
-            expect(typeof hashed).toBe('string');
+        test('should throw an error for invalid message', () => {
+            const invalidMessage = null;
+            expect(() => {
+                encryptHs256(invalidMessage, validKey);
+            }).toThrow('Invalid message');
         });
 
-        it('should throw an error if data is missing', () => {
-            expect(() => {
-                encryptHs256(undefined, secret);
-            }).toThrow('Missing data');
-        });
-
-        it('should throw an error if secret is missing', () => {
-            expect(() => {
-                encryptHs256(message, undefined);
-            }).toThrow('Missing secret');
+        test('should encrypt the message with a valid key', () => {
+            const encrypted = encryptHs256(validMessage, validKey);
+            expect(encrypted).toBeDefined();
+            expect(encrypted).not.toBe(validMessage);
         });
     });
 });
