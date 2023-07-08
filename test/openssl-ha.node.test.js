9263a0070c49428f06225ff3ceddc4a7577dbb03
Nhahan <kisy324@naver.com>
2023-07-09 02:48:39 +0900

    test: openssl-ha.node

--- test/openssl-ha.node.test.js
+++ test/openssl-ha.node.test.js
@@ -1,22 +1,58 @@
 const addon = require('../src/openssl-ha.node');
 
-describe('Native Addon', () => {
-    const message = 'Hello, World!';
-    const key = '12345678901234567890123456789012';
+const { encryptAes256, decryptAes256, encryptHs256 } = addon;
 
-    test('The decrypted message should be the same as the original message before encryption', () => {
-        const encrypted = addon.encryptAes256(message, key);
-        const decrypted = addon.decryptAes256(encrypted, key);
-        console.log('encrypted: ', encrypted, '\ndecrypted: ', decrypted);
+describe('OpenSSL-HA', () => {
+    let validMessage;
+    let validKey;
 
-        expect(decrypted).toEqual(message);
+    beforeEach(() => {
+        validMessage = 'Hello, World!';
+        validKey = '0123456789abcdef0123456789abcdef';
     });
 
-    test('Generates HMAC correctly', () => {
-        const encrypted = addon.encryptHs256(message, key);
-        console.log('encrypted: ', encrypted);
+    describe('encryptAes256', () => {
+        test('should encrypt the message with a valid key', () => {
+            const encrypted = encryptAes256(validMessage, validKey);
+            expect(encrypted).toBeDefined();
+            expect(encrypted).not.toBe(validMessage);
+        });
 
-        expect(encrypted).toBeDefined();
-        expect(typeof encrypted).toBe('string');
+        test('should throw an error for invalid message', () => {
+            const invalidMessage = null;
+            expect(() => {
+                encryptAes256(invalidMessage, validKey);
+            }).toThrow('Invalid message.');
+        });
+
+        test('should throw an error for invalid key', () => {
+            const invalidKey = null;
+            expect(() => {
+                encryptAes256(validMessage, invalidKey);
+            }).toThrow('Invalid key.');
+        });
+    });
+
+    describe('decryptAes256', () => {
+        test('should decrypt the ciphertext with a valid key', () => {
+            const encrypted = encryptAes256(validMessage, validKey);
+            const decrypted = decryptAes256(encrypted, validKey);
+            expect(decrypted).toBe(validMessage);
+        });
+    });
+
+    describe('encryptHs256', () => {
+        test('should throw an error for invalid message', () => {
+            const invalidMessage = null;
+            expect(() => {
+                encryptHs256(invalidMessage, validKey);
+            }).toThrow('Invalid message');
+        });
+
+        test('should encrypt the message with a valid key', () => {
+            const encrypted = encryptHs256(validMessage, validKey);
+            expect(encrypted).toBeDefined();
+            expect(encrypted).not.toBe(validMessage);
+        });
     });
 });
